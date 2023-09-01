0c5981ca1d69ebfb6de40256b35fc36e7981dff2
Achraf Mataich <achraf.mataich@outlook.com>
2023-09-01 19:52:47 +0100

    Update home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "", redirectTo: '/home', pathMatch: "full" },
+  { path: "projects", component: ProjectsComponent },
+  /* { path: "projects/:id", component: ProjectDetailsComponent }, */
+  { path: "**", component: FourOhFourComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ProjectDetailsComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
