685d5592c1f12c1ea0ff52a86828bc652b9969d1
Achraf Mataich <achraf.mataich@outlook.com>
2023-11-12 16:08:31 +0100

    update vcard

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,38 +1,79 @@
-import { Component, OnInit } from '@angular/core';
-import VCard from 'vcard-creator';
-
+import { Component } from '@angular/core';
+import { VCard } from 'ngx-vcard';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  vcardData: string;
-  vcDataUrl: string = "";
-
-  /**
-   *
-   */
-  constructor() {
-    const vc = new VCard('vcard');
-    vc.addName("MATAICH", "Achraf", "", "", "");
-    vc.addAddress("N 13 RUE 1 LOTS ERRACHIDIA RTE AIN CHKEF FES", "", "", "FES", "FES-MEKNES", "30040", "Morocco", "HOME");
-    vc.addPhoneNumber("+212618387787", "CELL");
-    vc.addPhoneNumber("+212770060084", "WHATSAPP");
-    vc.addEmail("achraf.mataich@outlook.com", "WORK");
-    vc.addSocial("https://www.linkedin.com/in/achraf-mataich", "Linkedin", "achraf mataich");
-    vc.addURL("achrafmataich.github.io/portfolio/", "WORK");
-
-    this.vcardData = vc.toString();
+  _data: VCard = {
+    name: {
+      firstNames: "Achraf",
+      lastNames: "Mataich"
+    },
+    address: [
+      {
+        street: "N 13 Rue 1 Lots Errachidia Rte Ain Chkef, Fes",
+        region: "Fes-Meknes",
+        postalCode: "30040",
+        param:
+        {
+          geo: "34.014106,-5.014200",
+          type: "home"
+        },
+        countryName: "Morocco",
+        label: "home"
+      }
+    ],
+    birthday: new Date(1999, 1, 20),
+    email: [
+      {
+        value: "achraf.mataich@outlook.com",
+        param: {
+          type: "work"
+        }
+      },
+      {
+        value: "achrafmataich99@gmail.com",
+        param: {
+          type: "home"
+        }
+      }
+    ],
+    gender: {
+      sex: "M"
+    },
+    geoPosition: "34.014106,-5.014200",
+    language: [
+      "French",
+      "English",
+      "Arabic"
+    ],
+    socialUrls: [
+      "https://www.linkedin.com/in/achraf-mataich/"
+    ],
+    telephone: [
+      {
+        value: "+212618387787",
+        param: {
+          type: "cell"
+        }
+      },
+      {
+        value: "+212770060084",
+        param: {
+          type: ["voice", "video"]
+        }
+      }
+    ]
   }
 
-  ngOnInit(): void {
-    this.vcDataUrl = this.generateDataUri(this.vcardData);
+  get data(): VCard {
+    return this._data;
   }
-
   // Generate a data URI from the vCard data
-  generateDataUri(data: string): string {
+  generateDataUri(data: string) {
     const blob = new Blob([data], { type: 'text/vcard' });
     return URL.createObjectURL(blob);
   }
